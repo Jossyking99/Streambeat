46164ecf781a8930fb0e4c53fa824482dc9fbf87
Ajoe <ijeborjoe@gmail.com>
2024-04-07 19:33:40 +0100

    add modify

--- mychat/settings.py
+++ mychat/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-cly%(5-34f8d1pc_fi*l_p@m3^x%#a$!iq(yu=s&&ez%-_pk$3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['127.0.0.1','.vercel.app']
 
 
 # Application definition
